f23ee55bfa9385ed1ebf358b302b9fdfd2f8385b
Owen Sorber <38570999+owsorber@users.noreply.github.com>
2019-02-08 20:17:08 -0500

    original commit

---
+++ footer.js
@@ -0,0 +1,13 @@
+//footer.js
+/* Script for the year in the copyright footer on every page */
+
+var startingYear = 2018; //gather the starting year of work featured in website
+var year = new Date().getFullYear(); //get the current year
+var footerYear = document.getElementById("footer-date"); //access "footer-date" id in html file
+
+// Conditional for applying today's year to the footer
+if (year == startingYear) {
+    footerYear.innerHTML = startingYear;
+} else {
+    footerYear.innerHTML = startingYear + "-" + year;
+}
