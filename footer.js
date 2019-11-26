b781940ad178e021dba19bd3d0d76279809400b7
Dylan <dyli@students.natickps.org>
2019-11-26 15:05:42 -0500

    big maintenance
    cleanup of:
    nav formatting for consistency
    commenting out of hero image for replacement

--- footer.js
+++ footer.js
@@ -1,7 +1,7 @@
 //footer.js
 /* Script for the year in the copyright footer on every page */
 
-var startingYear = 2018; //gather the starting year of work featured in website
+var startingYear = 2019; //gather the starting year of work featured in website
 var year = new Date().getFullYear(); //get the current year
 var footerYear = document.getElementById("footer-date"); //access "footer-date" id in html file
 
